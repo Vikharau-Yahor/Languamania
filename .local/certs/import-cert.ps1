663785c0327c7afc4a5e896705681fc8014c0044
Yahor Vikharau <egor.vikharau@gmail.com>
2024-03-25 09:56:19 +0100

    * enable https on BE server + FE server * add import/generate ssl tools * use the same SSL cert for both servers

---
+++ .local/certs/import-cert.ps1
@@ -0,0 +1,2 @@
+dotnet dev-certs https --clean --import .\lang-local-cert.pfx -p 'Password'
+dotnet dev-certs https --trust
--- Languamania.Server/Program.cs
+++ Languamania.Server/Program.cs
@@ -1,23 +1,49 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using System.Security.Cryptography.X509Certificates;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions
+{
+    SslProtocols = System.Security.Authentication.SslProtocols.Tls12,
+    ClientCertificateMode = ClientCertificateMode.AllowCertificate,
+    ServerCertificate = new X509Certificate2("..\\.local\\certs\\lang-local-cert.pfx", "Password")
 
+};
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.ConfigureEndpointDefaults(listenOptions =>
+        listenOptions.UseHttps(httpsConnectionAdapterOptions)
+    );
+});
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
